371b1d11d938d1b81fe3fe35b2b3dcee72f7dd29
Matt Conway <matthew.f.conway@gmail.com>
2014-02-10 21:04:37 -0500

    update eclipse classpath template and renderer fn

--- src/leiningen/idefiles.clj
+++ src/leiningen/idefiles.clj
@@ -45,33 +45,40 @@
        (map (partial relative-path base-path))
        vec))
 
-(defn relativize-paths
-  [project accumulator-key path-keys src-accumulator-key src-path-keys]
-  (let [accumulate-all (fn [m] (merge m {accumulator-key (mapcat m path-keys)}))
-        accumulate-src (fn [m] (merge m {src-accumulator-key (mapcat m src-path-keys)}))]
-   (->> path-keys
-        (map (fn [k] {k (rel-paths (get project :root ".")
-                                  (get project k))}))
-        (apply merge project)
-        (accumulate-all)
-        (accumulate-src))))
-
 (defn echo
   "Print and return the argument. Use for debugging."
   [x]
   (println "---------------")
   (pp/pprint x)
   x)
+(defn relativize-paths
+  [project accumulator-key path-keys src-accumulator-key src-path-keys]
+  (let [accumulate-all (fn [m] (merge m {accumulator-key (mapcat m path-keys)}))
+        accumulate-src (fn [m] (merge m {src-accumulator-key (mapcat m src-path-keys)}))]
+   (->> path-keys
+        (map (fn [k] {k (rel-paths (get project :root ".")
+                                  (let [path-or-paths (get project k)]
+                                    (if (seq? path-or-paths)
+                                      path-or-paths
+                                      (list path-or-paths)))
+                                  )}))
+        echo
+        (apply merge project)
+        (accumulate-all)
+        echo
+        (accumulate-src))))
+
+
 
 (defn idefiles
   "Generate IDE files for a Leiningen project"
   [project ide]
   (let [only-source-keys [:java-source-paths :source-paths :resource-paths]
-        all-source-keys (concat only-source-keys [:test-paths])]
+        all-source-keys (concat only-source-keys [:test-paths :compile-path ])]
     (-> {:classpath-jars (classpath-jars project)}
         (merge (select-keys project (concat [:group :name] all-source-keys)))
-        ;; echo
+         echo
         (relativize-paths :all-source-paths all-source-keys
                           :only-source-paths only-source-keys)
-        ;; echo
+         echo
         (nif/ide-files ide))))
